0b3babb99edc4fbf51fd354c96b933cf203fbab7
sourav ku mohanta <souravku457@gmail.com>
2020-08-23 23:11:48 +0530

    final commit
    build file

---
+++ build/precache-manifest.3d0c03283ffb0a1875fe925f5af8abbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546d41c97903b415b48f053ff3ff2e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e16587c7a1a918314f16",
+    "url": "/static/css/main.248cf42c.chunk.css"
+  },
+  {
+    "revision": "b6742956d0d73730b709",
+    "url": "/static/js/2.ea59d149.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ea59d149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16587c7a1a918314f16",
+    "url": "/static/js/main.576b31ba.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
